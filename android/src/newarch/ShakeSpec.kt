f67453c66ac966e2ed5a10b0ef87e2e22790d876
Jeroen van Dijk <dijkvan@gmail.com>
2025-05-27 19:24:10 +0200

    chore(android): update generated files

--- android/src/newarch/ShakeSpec.kt
+++ android/src/newarch/ShakeSpec.kt
@@ -3,6 +3,7 @@ package com.shake
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.Promise
 import com.facebook.react.module.annotations.ReactModule
+import com.shake.NativeShakeSpec
 
 @ReactModule(name = ShakeModule.NAME)
 abstract class ShakeSpec internal constructor(context: ReactApplicationContext) :
